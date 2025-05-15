7e0955b2e82073730220a0df4cc7593d087a8185
Zacharia Ndinguri <zacthuku@gmail.com>
2025-05-15 08:25:25 +0300

    method lab

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,48 @@
 class Song:
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+    
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.add_song_to_count()
+        Song.genres.append(genre)
+        Song.add_to_genres()
+        Song.artists.append(artist)
+        Song.add_to_artists()
+        Song.add_to_genre_count(genre)
+        Song.add_to_artist_count(artist)
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls):
+        # Remove duplicates while preserving order
+        cls.genres = list(dict.fromkeys(cls.genres))
+
+    @classmethod
+    def add_to_artists(cls):
+        # Remove duplicates while preserving order
+        cls.artists = list(dict.fromkeys(cls.artists))
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
+
+        
     pass
